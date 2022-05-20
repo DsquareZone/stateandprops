ab4048e91b78fed45af3f339c580e8172b990fb6
Surya Prakash <97003032+DsquareZone@users.noreply.github.com>
2022-05-20 15:31:09 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-const App = () => {
-  let name = "DSquareZone";
-  return (
-    <div>
-      <center>
-        <h1> Welcome {name} </h1>
-      </center>
-    </div>
-  );
-};
+import React from "react";
+import "./styles.css";
+
+class App extends React.Component {
+  state = {
+    name: "User",
+    age: 23
+  };
+  render() {
+    return (
+      <div>
+        <center>
+          <h1>
+            Name : {this.state.name} and Age: {this.state.age}
+          </h1>
+        </center>
+      </div>
+    );
+  }
+}
 export default App;
