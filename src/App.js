81548e9287b010b641eb038c725d46e153bd01fa
Surya Prakash <97003032+DsquareZone@users.noreply.github.com>
2022-05-20 15:11:49 +0530

    State Commited

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 
-const App = () => {
-  const [data, setData] = useState({
-    email: "",
-    password: ""
-  });
-  const changeHandler = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value });
+class App extends React.Component {
+  state = {
+    name: "User",
+    age: 23
   };
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (data.password.length < 5) {
-      alert("provide must be more than 5 characters");
-    } else {
-      console.log(data);
-    }
-  };
-  return (
-    <div className="App">
-      <center>
-        <form onSubmit={submitHandler}>
-          <label> Email </label>
-          <input
-            type="email"
-            name="email"
-            onChange={changeHandler}
-          /> <br /> <br />
-          <label> Password </label>
-          <input
-            type="password"
-            name="password"
-            onChange={changeHandler}
-          />{" "}
-          <br /> <br />
-          <input type="submit" value="Login" />
-        </form>
-      </center>
-    </div>
-  );
-};
-
+  render() {
+    return (
+      <div>
+        <center>
+          <h1>
+            Name : {this.state.name} and Age: {this.state.age}
+          </h1>
+        </center>
+      </div>
+    );
+  }
+}
 export default App;
